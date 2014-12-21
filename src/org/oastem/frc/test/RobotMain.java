8e53a4044925f450bc43e2366ca008d569af023e
Michael <michael_9630@yahoo.com>
2014-12-20 18:40:43 -0800

    Work on gyro and encoder

--- src/org/oastem/frc/test/RobotMain.java
+++ src/org/oastem/frc/test/RobotMain.java
@@ -11,6 +11,7 @@ package org.oastem.frc.test;
 import edu.wpi.first.wpilibj.*;
 import org.oastem.frc.control.*;
 import org.oastem.frc.Debug;
+import org.oastem.frc.sensor.*;
 
 
 /**
@@ -46,15 +47,24 @@ public class RobotMain extends SimpleRobot {
     
     private final int RUN_TIME = 5000;
     
+    private Encoder encoder;
+    private ADW22307Gyro gyro;
+    
+    private final int ENCODER_PORT_A = 1;
+    private final int ENCODER_PORT_B = 2;
+    private final int GYRO_PORT = 3;
+    
     
     public void robotInit(){
-        ds = DriveSystem.getInstance();
-        ds.initializeDrive(LEFT_DRIVE_PORT, RIGHT_DRIVE_PORT);
+        //ds = DriveSystem.getInstance();
+        //ds.initializeDrive(LEFT_DRIVE_PORT, RIGHT_DRIVE_PORT);
         
         js = new Joystick(FIRST_JOYSTICK);
-        js2 = new Joystick(SECOND_JOYSTICK);
+        encoder = new Encoder(ENCODER_PORT_A, ENCODER_PORT_B);
+        gyro = new ADW22307Gyro(GYRO_PORT);
         
-        lim = new DigitalInput(LIMIT_SWITCH_PORT);
+        
+       
         
         motor1 = new Jaguar(JAGUAR_PORT);
         
@@ -65,19 +75,7 @@ public class RobotMain extends SimpleRobot {
     /**
      * This function is called once each time the robot enters autonomous mode.
      */
-    public void autonomous() {
-        //move forward
-        ds.tankDrive(AUTO_SPEED, AUTO_SPEED);
-        Timer.delay(5000);
-        //turn clockwise
-        ds.tankDrive(AUTO_SPEED, -AUTO_SPEED);
-        Timer.delay(1000);
-        //move forward
-        ds.tankDrive(AUTO_SPEED, AUTO_SPEED);
-        Timer.delay(2000);
-        //stop
-        ds.tankDrive(0, 0);
-    }
+    
 
     /**
      * This function is called once each time the robot enters operator control.
@@ -88,7 +86,7 @@ public class RobotMain extends SimpleRobot {
         boolean motorStart = false;
         while(isEnabled() && isOperatorControl()){
             currentTime = System.currentTimeMillis();
-            debug[1] = "Drive Speed: " + js.getY() + ", " + js2.getY();
+            /*debug[1] = "Drive Speed: " + js.getY() + ", " + js2.getY();
             ds.tankDrive(js.getY(), js2.getY());
             
             if (js.getRawButton(JOYSTICK_BUTTON) || motorStart){
@@ -105,7 +103,12 @@ public class RobotMain extends SimpleRobot {
                     debug[2] = "Still going at time " + currentTime;
                 }
             }
+            */
             
+            motor1.set(js.getY());
+            debug[0] = "Rate: " + encoder.getRate();
+            debug[1] = "Angle: " + gyro.getAngle();
+            debug[2] = "how to do";
             Debug.log(debug);
         }
     }
