b0b132f963ce9fd802c538b8c8fde27821dd4f4a
Joy <joyhsu0504@gmail.com>
2014-12-29 12:14:42 -0800

    lol image stuff

---
+++ src/org/oastem/frc/sensor/imagePoint.java
@@ -0,0 +1,102 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.oastem.frc.imaging;
+
+import com.sun.squawk.util.MathUtils;
+
+/**
+ * This class is gonna contain ALL of the possible data we can get
+ * on each data point we get. 
+ * 
+ * Tons of variables. You have been warned.
+ * 
+ * @author KTOmega
+ */
+public class Point {
+    public double rectangularity;
+    public double aspectRatioInner;
+    public double aspectRatioOuter;
+    public double xEdge;
+    public double yEdge;
+    public double x;
+    public double y;
+    public int label = 0;
+    public int marked = -1;
+    
+    public boolean hot = false;
+    
+    // Oh dear, oh my.
+    // The things I do to make code look nice.
+    public static final int INVALID = -1;
+    public static final int LEFT = 0;
+    public static final int RIGHT = 1;
+    public static final int HORIZONTAL = 0;
+    public static final int VERTICAL = 1;
+    
+    // 0 => left, 1 => right
+    private int side = INVALID;
+    
+    // 0 => horizontal, 1 => vertical
+    private int orientation = INVALID;
+    
+    public Point(int label, double x, double y) {
+        this(x, y);
+        this.setLabel(label);
+    }
+    
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public double getX() { return this.x; }
+    public double getY() { return this.y; }
+    
+    public int getOrientation() { return this.orientation; }
+    public int getSide() { return this.side; }
+    
+    public boolean isLeft() {
+        return side == 0;
+    }
+    
+    public boolean isRight() {
+        return side == 1;
+    }
+    
+    public double getAspectRatio() {
+        // ???? WHICH VARIABLE IS THE ACTUAL ASPECT RATIO?
+        return aspectRatioOuter;
+    }
+    
+    public boolean hasAspect(double aspect, double error) {
+        return Math.abs(this.getAspectRatio() - aspect) <= error;
+    }
+    
+    public void setOrientation(int newOr) {
+        this.orientation = newOr;
+    }
+    
+    public void setSide(int newSide) {
+        this.side = newSide;
+    }
+    
+    public int getLabel() { return this.label; }
+    public void setLabel(int newLabel) { this.label = newLabel; }
+    
+    public boolean isMarked() { return this.marked != -1; }
+    public int getMarker() { return this.marked; }
+    public void setMarker(int mark) { this.marked = mark; }
+    
+    public boolean isHot() { return hot; }
+    public void setHot(boolean orNot) { this.hot = orNot; }
+    
+    public static double calculateDistance(Point p1, Point p2) {
+        return Math.sqrt(MathUtils.pow(p2.getX() - p1.getX(), 2) + MathUtils.pow(p2.getY() - p1.getY(), 2));
+    }
+    
+    public String toString() {
+        return "(" + this.getX() + ", " + this.getY() + ")";
+    }
+}
---
+++ src/org/oastem/frc/sensor/imageProcessing.java
@@ -0,0 +1,135 @@
+private void imageProcessing() {
+        try {
+            // 43:32
+            //ColorImage image = camera.getImage();     // comment if using stored images
+            ColorImage image;                           // next 2 lines read image from flash on cRIO
+            image = camera.getImage();
+            //image = new RGBImage("/testImage.jpg");		// get the sample image from the cRIO flash
+            BinaryImage thresholdImage = image.thresholdRGB(0, 50, 100, 255, 0, 50);
+            //BinaryImage thresholdImage = image.thresholdHSV(60, 100, 90, 255, 20, 255);   // keep only red objects
+            //thresholdImage.write("/threshold.bmp");
+            BinaryImage convexHullImage = thresholdImage.convexHull(false);          // fill in occluded rectangles
+            //convexHullImage.write("/convexHull.bmp");
+            BinaryImage filteredImage = convexHullImage.particleFilter(cc);           // filter out small particles
+            //filteredImage.write("/filteredImage.bmp");
+            //SmartDashboard.
+
+            //iterate through each particle and score to see if it is a target
+            Point scores[] = new Point[filteredImage.getNumberParticles()];
+            for (int i = 0; i < scores.length; i++) {
+                ParticleAnalysisReport report = filteredImage.getParticleAnalysisReport(i);
+                scores[i] = new Point(i, report.center_mass_x_normalized, report.center_mass_y_normalized);
+
+                scores[i].rectangularity = ImagingUtils.scoreRectangularity(report);
+                scores[i].aspectRatioOuter = ImagingUtils.scoreAspectRatio(filteredImage, report, i, true);
+                scores[i].aspectRatioInner = ImagingUtils.scoreAspectRatio(filteredImage, report, i, false);
+                scores[i].xEdge = ImagingUtils.scoreXEdge(thresholdImage, report);
+                scores[i].yEdge = ImagingUtils.scoreYEdge(thresholdImage, report);
+
+                if (scores[i].aspectRatioOuter > 1.0) {
+                    // Width > height, it's the horizontal goal
+                    horzCenterMassX = report.center_mass_x_normalized;
+                    horzCenterMassY = report.center_mass_y_normalized;
+                    System.out.println(i + ": HorizGoal cx: " + report.center_mass_x_normalized + " cy: "
+                            + report.center_mass_y_normalized);
+
+                } else {
+                    // Height > width, it's the vertical goal
+                    vertCenterMassX = report.center_mass_x_normalized;
+                    vertCenterMassY = report.center_mass_y_normalized;
+                    System.out.println(i + ": VertGoal cx: " + report.center_mass_x_normalized + " cy: "
+                            + report.center_mass_y_normalized
+                            + " h: " + (report.boundingRectHeight / (double) report.imageHeight));
+                    vertHeight = report.boundingRectHeight;
+                    // This function seems to work only between 2-5 meters.
+                    //double dist = 129166.84601965 * ( MathUtils.pow(report.boundingRectHeight, -1.172464652462));
+                    System.out.println(report.boundingRectHeight);
+                    //System.out.println( (347.5 * report.boundingRectHeight) / 92.0 );
+                }
+
+
+
+                // The following code will only store the initial readings.
+                /*
+                 * if (massCenters == null) { // We'll only take in the initial
+                 * reading. massCenters = scores; }//
+                 */
+
+                // in discovering distance. ...
+                // y = distance to target (to find)
+                // x = sample distance (i.e. 10 meters)
+                // h = sample height of target (corresponding to sample distance)
+                // z = height of target
+                // y = hz / x
+
+                // h = 92 px
+                // x = 347.5 cm
+                // ----------------------------------
+                // DISTANCE til full view of vision targets: 78.9 inches == 200 cm!!! 2 m
+                // x = FOV/140 //credits to Spring
+                // x = distance from wall to robot
+                // FOV = bounding rect width --> width from edge of horzgoal to other edge 
+                // TEST THIS
+                // Put robot 2 meters from the vision targets and measure the pixel width of the image
+                // (or boundingRectWidth) (from the edges of the goals
+
+
+
+                /*
+                 * if(scoreCompare(scores[i], false)) {
+                 * System.out.println("particle: " + i + "is a High Goal
+                 * centerX: " + report.center_mass_x_normalized + "centerY: " +
+                 * report.center_mass_y_normalized);
+                 * System.out.println("Distance: " +
+                 * computeDistance(thresholdImage, report, i, false)); } else if
+                 * (scoreCompare(scores[i], true)) {
+                 * System.out.println("particle: " + i + "is a Middle Goal
+                 * centerX: " + report.center_mass_x_normalized + "centerY: " +
+                 * report.center_mass_y_normalized);
+                 * System.out.println("Distance: " +
+                 * computeDistance(thresholdImage, report, i, true)); } else {
+                 * System.out.println("particle: " + i + "is not a goal centerX:
+                 * " + report.center_mass_x_normalized + "centerY: " +
+                 * report.center_mass_y_normalized); }
+                 */
+                //System.out.println("rect: " + scores[i].rectangularity + "ARinner: " + scores[i].aspectRatioInner);
+                //System.out.println("ARouter: " + scores[i].aspectRatioOuter + "xEdge: " + scores[i].xEdge + "yEdge: " + scores[i].yEdge);	
+            }
+
+            massCenters = scores;
+            ImagingUtils.determineGoals(scores);
+
+            for (int i = 0; i < scores.length; i++) {
+                Point cur = scores[i];
+                String o = cur.getOrientation() == Point.INVALID ? "Invalid"
+                        : (cur.getOrientation() == Point.HORIZONTAL ? "Horiz" : "Vert");
+                String s = cur.getSide() == Point.INVALID ? "Invalid"
+                        : (cur.getSide() == Point.LEFT ? "Left" : "Right");
+
+                String h = cur.isHot() ? "Hot" : "NotHot";
+
+                if (cur.isHot()) {
+                    currentHotGoal = cur.getSide();
+                }
+
+                System.out.println("Goal " + i + ": " + o + " " + s + " " + h);
+            }
+
+            //public void checkRegion(
+            //System.out.println(ImagingUtils.isRightOrLeft(vertCenterMassX, vertCenterMassY, horzCenterMassX, horzCenterMassY) + "");
+
+            /**
+             * all images in Java must be freed after they are used since they
+             * are allocated out of C data structures. Not calling free() will
+             * cause the memory to accumulate over each pass of this loop.
+             */
+            filteredImage.free();
+            convexHullImage.free();
+            thresholdImage.free();
+            image.free();
+            System.out.println("-------");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        lastUpdate = System.currentTimeMillis();
+    }
