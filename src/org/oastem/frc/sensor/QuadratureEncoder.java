99c78d9803ee89493c340f466c2353b9dd9c6450
mduong15 <michael_9630@yahoo.com>
2015-01-12 17:16:01 -0800

    Set up Encoder wrapper methods

--- src/org/oastem/frc/sensor/QuadratureEncoder.java
+++ src/org/oastem/frc/sensor/QuadratureEncoder.java
@@ -14,18 +14,26 @@ import edu.wpi.first.wpilibj.Encoder;
  */
 public class QuadratureEncoder {
     private Encoder enc;
+    private double pulsesPerRevolution;
     
     public QuadratureEncoder(int channelA, int channelB)
     {
         enc = new Encoder(channelA, channelB);
     }
     
-    public QuadratureEncoder(int channelA, int channelB, boolean isReversed)
+    public QuadratureEncoder(int channelA, int channelB, double pulsesPerRev)
     {
-        enc = new Encoder(channelA, channelB, isReversed);
+        enc = new Encoder(channelA, channelB);
+        pulsesPerRevolution = pulsesPerRev;
     }
     
-    public QuadratureEncoder(int channelA, int channelB, int scaleValue)
+    public QuadratureEncoder(int channelA, int channelB, boolean isReversed, double pulsesPerRev)
+    {
+        enc = new Encoder(channelA, channelB, isReversed);
+        pulsesPerRevolution = pulsesPerRev;
+    }
+    
+    public QuadratureEncoder(int channelA, int channelB, int scaleValue, double pulsesPerRev)
     {
         if (scaleValue == 1)
             enc = new Encoder(channelA, channelB, false, CounterBase.EncodingType.k1X);
@@ -35,9 +43,12 @@ public class QuadratureEncoder {
             enc = new Encoder(channelA, channelB, false, CounterBase.EncodingType.k4X);
         else
             enc = new Encoder(channelA, channelB, false, CounterBase.EncodingType.k4X);
+        
+        pulsesPerRevolution = pulsesPerRev;
     }
     
-    public QuadratureEncoder(int channelA, int channelB, boolean isReversed, int scaleValue)
+    public QuadratureEncoder(int channelA, int channelB, boolean isReversed,
+                            int scaleValue, double pulsesPerRev)
     {
         if (scaleValue == 1)
             enc = new Encoder(channelA, channelB, isReversed, CounterBase.EncodingType.k1X);
@@ -47,9 +58,59 @@ public class QuadratureEncoder {
             enc = new Encoder(channelA, channelB, isReversed, CounterBase.EncodingType.k4X);
         else
             enc = new Encoder(channelA, channelB, isReversed, CounterBase.EncodingType.k4X);
+        
+        pulsesPerRevolution = pulsesPerRev;
     }
     
     
+    public void free()
+    {
+        enc.free();
+    }
     
+    public int get()
+    {
+        return enc.get();
+    }
+    
+    public double getDistance()
+    {
+        return enc.getDistance();
+    }
+    
+    public double getRate()
+    {
+        return enc.getRate();
+    }
+    
+    public int getRaw()
+    {
+        return enc.getRaw();
+    }
+    
+    public boolean isGoingForward()
+    {
+        return enc.getDirection();
+    }
+    
+    public boolean isStopped()
+    {
+        return enc.getStopped();
+    }
+    
+    public void reset()
+    {
+        enc.reset();
+    }
+    
+    public void setDistancePerPulse(int wheelCircumference)
+    {
+        enc.setDistancePerPulse(wheelCircumference / pulsesPerRevolution);
+    }
+    
+    public void setDistanceWithRatio(int distancePerPulse, double gearRatio)
+    {
+        enc.setDistancePerPulse(distancePerPulse * gearRatio);
+    }
     
 }
