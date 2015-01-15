4a4e3efd9c251338f6ec4d44496f7b69d4496ff5
mduong15 <michael_9630@yahoo.com>
2015-01-14 17:56:56 -0800

    Comment the constructors for the encoder wrapper

--- src/org/oastem/frc/sensor/QuadratureEncoder.java
+++ src/org/oastem/frc/sensor/QuadratureEncoder.java
@@ -14,14 +14,32 @@ import edu.wpi.first.wpilibj.Encoder;
  */
 public class QuadratureEncoder {
     private final Encoder enc;
+    
+    //This is different depending on encoder and motors (account for gear ratio).
+    //This must be figured out by using docs and specs.
+    //(If desperate, just have the motor spin one rotation and see how many pulses it is.)
     private double pulsesPerRevolution;
     
+    
+    /**
+     * Creates an encoder using the two SIG inputs (A and B) 
+     * Default to use to just get encoder readings and not use getDistance().
+     * @param channelA I/0 SIG A 
+     * @param channelB I/0 SIG B
+     */
     public QuadratureEncoder(int channelA, int channelB)
     {
         enc = new Encoder(channelA, channelB);
         enc.start();
     }
     
+    /**
+     * Creates an encoder using the two SIG inputs (A and B) 
+     * pulsesPerRev is used for getDistance() methods
+     * @param channelA I/0 SIG A 
+     * @param channelB I/O SIG B
+     * @param pulsesPerRev Number of pulses for a revolution of the motor (look at instance variable)
+     */
     public QuadratureEncoder(int channelA, int channelB, double pulsesPerRev)
     {
         enc = new Encoder(channelA, channelB);
@@ -29,6 +47,15 @@ public class QuadratureEncoder {
         enc.start();
     }
     
+    /**
+     * Creates an encoder using the two SIG inputs (A and B) 
+     * Also allows the encoder to be reversed
+     * pulsesPerRev is used for getDistance() methods
+     * @param channelA I/0 SIG A 
+     * @param channelB I/O SIG B
+     * @param isReversed When true, returned values are inverted
+     * @param pulsesPerRev Number of pulses for a revolution of the motor (look at instance variable)
+     */
     public QuadratureEncoder(int channelA, int channelB, boolean isReversed, double pulsesPerRev)
     {
         enc = new Encoder(channelA, channelB, isReversed);
@@ -36,6 +63,17 @@ public class QuadratureEncoder {
         enc.start();
     }
     
+    /**
+     * Creates an encoder using the two SIG inputs (A and B) 
+     * Also allows the encoder to be reversed
+     * Can also control the difference between getRaw and get values
+     * pulsesPerRev is used for getDistance() methods
+     * @param channelA I/0 SIG A 
+     * @param channelB I/O SIG B
+     * @param isReversed When true, returned values are inverted
+     * @param scaleValue getRaw() values are divided by multiples of 1, 2, or 4 to increase accuracy
+     * @param pulsesPerRev Number of pulses for a revolution of the motor (look at instance variable)
+     */
     public QuadratureEncoder(int channelA, int channelB, boolean isReversed,
                             int scaleValue, double pulsesPerRev)
     {
@@ -54,6 +92,15 @@ public class QuadratureEncoder {
         enc.start();
     }
     
+    /**
+     * Creates an encoder using the two SIG inputs (A and B) 
+     * Can also control the difference between getRaw and get values
+     * pulsesPerRev is used for getDistance() methods
+     * @param channelA I/0 SIG A 
+     * @param channelB I/O SIG B
+     * @param scaleValue getRaw() values are divided by multiples of 1, 2, or 4 to increase accuracy
+     * @param pulsesPerRev Number of pulses for a revolution of the motor (look at instance variable)
+     */
     public QuadratureEncoder(int channelA, int channelB, int scaleValue, double pulsesPerRev)
     {
         this(channelA, channelB, false, scaleValue, pulsesPerRev);
